3f7c51f9870b802c897d0d60584d0c9db1e477d4
KlaasJan Elzinga <klaasjanelzinga@me.com>
2019-04-15 22:01:01 +0200

    css -> main.css rename on deploy

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -3,7 +3,7 @@ set -x
 set -e
 
 npm run-script build
-sed  -i '' 's/main\..*\.chunk\.css/main.css/' build/index.html
+sed  -i 's/main\..*\.chunk\.css/main.css/' build/index.html
 mv build/static/css/main.*.chunk.css build/static/css/main.css
 
 rm -rf ../static/build && mv build ../static
