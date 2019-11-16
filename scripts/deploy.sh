170a978cf7bfff1a54cda747b8ad2ff10e1dd679
KlaasJan Elzinga <klaasjanelzinga@me.com>
2019-11-16 20:35:21 +0100

    new deployer name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,5 @@ docker run -v $project_dir:/usr/src/app rss-aggregator-app scripts/build.sh
 export version="develop-`git rev-parse --short HEAD`"
 echo "Version: $version"
 
-gcloud auth activate-service-account deployer@rss-aggregator-236707.iam.gserviceaccount.com  --key-file secrets/deployer.json --project rss-aggregator-236707
 gcloud app deploy --version $version --project rss-aggregator-236707 app.yaml
 gcloud app deploy --version $version --project rss-aggregator-236707 cron.yaml
